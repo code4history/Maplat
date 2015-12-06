8d4fe18324a1b55f02ebff362f1e5fa06afd9e2f
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-12-06 13:54:33 +0900

    現在地取得 => センター移動

--- js/implementation.js
+++ js/implementation.js
@@ -24,6 +24,8 @@ var tps = new ThinPlateSpline({
                 hereMarker[1].setLatLng(tgtll);
             } else {
                 hereMarker[1] = L.marker(tgtll,{icon:hereIcon}).addTo(map[1]);
+                map[1].panTo(tgtll);
+                map[1].setZoom(5);
             }
         } else if (options.target == "drag") {
             map[isRev].panTo(tgtll);
@@ -221,6 +223,8 @@ $(window).load(function(){
             hereMarker[0].setLatLng(latlng);
         } else {
             hereMarker[0] = L.marker(latlng,{icon:hereIcon}).addTo(map[0]);
+            map[0].panTo(latlng);
+            map[0].setZoom(17);
         }
         var merc = map[0].ll2xy(new L.LatLng(position.coords.latitude,position.coords.longitude));
         var tgtxy = tps.transform([merc.x,merc.y],1,{"target":"here"});            
