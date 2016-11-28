bf7f9d398b64095f88b927d5a7c24167b4879a06
OHTSUKA Ko-hei <kochizufan@gmail.com>
2016-11-28 22:15:16 +0900

    サーバ側で変換バイナリテーブル作るためのスクリプト作成（by node.js）

--- js/thinplatespline.js
+++ js/thinplatespline.js
@@ -7733,62 +7733,6 @@ if (shouldRunNow) {
 return ThinPlateSpline;
 })();
 
-/*var tps = new ThinPlateSpline();
-tps.add_point([100,100], [200, 200]);
-tps.add_point([200,200], [400, 400]);
-tps.add_point([150,150], [320, 350]);
-//tps.add_point(100,200,[205,412]);
-tps.solve();
-var ord = tps.transform([160,160]);
-//console.log('honyo');
-console.log(ord);
-var rev = tps.transform(ord,true);
-//console.log('honyo');
-console.log(rev);
-
-var serial = tps.serialize();
-
-var fs = require('fs');
-
-var buf = new Buffer(serial.length);
-for (var i = 0; i < serial.length; ++i) {
-  buf[i] = serial[i];
-}
-fs.writeFileSync('./serial.bin',buf);
-
-var buf2 = fs.readFileSync('./serial.bin');
-var abuf = new ArrayBuffer(buf2.length);
-var serial2 = new Uint8Array(abuf);
-for (var i = 0; i < buf2.length; ++i) {
-  serial2[i] = buf2[i];
-}
-
-var tps2 = new ThinPlateSpline();
-
-tps2.deserialize(serial2);
-
-var ord2 = tps2.transform([160,160]);
-//console.log('honyo');
-console.log(ord2);
-var rev2 = tps2.transform(ord2,true);
-//console.log('honyo');
-console.log(rev2);
-
-/*var narat = new ThinPlateSpline();
-var narap = require('./nara_points.json');
-narat.push_points(narap);
-
-narat.solve();
-
-var naras = narat.serialize();
-
-var narab = new Buffer(naras.length);
-for (var i = 0; i < naras.length; ++i) {
-  narab[i] = naras[i];
-}
-fs.writeFileSync('./nara_serial.bin',narab);*/
-
-
 if (typeof importScripts === 'function') {
   /* Worker loader */
   var tps = new ThinPlateSpline();
@@ -7860,3 +7804,7 @@ if (typeof importScripts === 'function') {
   };
 }
 
+exports = ThinPlateSpline;
+try {
+  module.exports = ThinPlateSpline;
+} catch (ex) { }
