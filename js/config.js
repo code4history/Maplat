1344a0459b79b0155137bebcd5de64fb0e78ec15
OHTSUKA Ko-hei <kochizufan@gmail.com>
2016-12-03 17:50:31 +0900

    #8 地図切り替えUIをブラッシュアップ (Slick使用) (#25)
    * Slick導入中
    
    * セレクタを消してSlickオンリーに。画像表示まではまだ。
    
    * Slickでサムネイル利用可能に

--- js/config.js
+++ js/config.js
@@ -8,6 +8,7 @@ require.config({
     paths: {
         "jquery" : "//code.jquery.com/jquery-1.11.2.min",
         "jui" : "//code.jquery.com/ui/1.10.3/jquery-ui.min",
+        "slick" : "//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min",
         "ol3" : "ol-debug",
         "ol-custom" : "ol-custom",
         //"ol3css" : "../css/ol",
@@ -21,6 +22,9 @@ require.config({
         },
         'jui' : { 
             deps: ['jquery'] 
+        },
+        'slick' : {
+            deps: ['jquery']
         },
         'ol' : {
             exports: 'ol'
