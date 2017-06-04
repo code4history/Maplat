ab5f2b93eb9a2fee473739e034e052dfc5c5bef6
OHTSUKA Ko-hei <kochizufan@gmail.com>
2017-06-04 10:01:41 +0900

    datacreator.js update for new data format

--- tools/datacreator.js
+++ tools/datacreator.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var path = require('path');
 var argv = require('argv');
-var readline = require('readline');
 var csvSync = require('csv-parse/lib/sync');
 
 var args = argv.option( [{
@@ -12,28 +11,64 @@ var args = argv.option( [{
     type: 'path',
     description: 'Defines source points file. QGIS or Maplat format file can be set.',
     example: "'datacreator.js --source=path' or 'datacreator.js -s path'"
-},{
+}, {
     name: 'output',
     short: 'o',
     type: 'path',
     description: 'Defines output Maplat format points file.',
     example: "'datacreator.js --output=path' or 'datacreator.js -o path'"
-},{
+}, {
     name: 'backward',
     short: 'b',
     type: 'path',
     description: 'Defines whether backward QGIS format points file is need or not. output file name can be set.',
     example: "'datacreator.js --backward[=path]' or 'datacreator.js -b [path]'"
-},{
+}, {
     name: 'tps',
-    short: 't',
+    short: 'T',
     type: 'path',
     description: 'Defines whether Thinplate Spline binary is need or not. output file name can be set.',
     example: "'datacreator.js --tps[=path]' or 'datacreator.js -t [path]'"
+}, {
+    name: 'title',
+    short: 't',
+    type: 'string',
+    description: 'Defines map data title. If transformation is forward, this option or "-O" option must be set.',
+    example: "'datacreator.js --title[=string]' or 'datacreator.js -t [string]'"
+}, {
+    name: 'size',
+    short: 'S',
+    type: 'csv,int',
+    description: 'Defines map size as "x,y". If transformation is forward, this option or "-O" option must be set.',
+    example: "'datacreator.js --size[=int,int]' or 'datacreator.js -S [int,int]'"
+}, {
+    name: 'year',
+    short: 'y',
+    type: 'int',
+    description: 'Defines map created year. If transformation is forward, this option or "-O" option must be set.',
+    example: "'datacreator.js --year[=int]' or 'datacreator.js -y [int]'"
+}, {
+    name: 'attr',
+    short: 'a',
+    type: 'string',
+    description: 'Defines map attribution. If transformation is forward, this option or "-O" option must be set.',
+    example: "'datacreator.js --attr[=string]' or 'datacreator.js -a [string]'"
+}, {
+    name: 'oldvalue',
+    short: 'O',
+    type: 'string',
+    description: 'Defines old value from previous file. If transformation is forward, this option or "-t,-S,-y,-a" options must be set.',
+    example: "'datacreator.js --oldvalue[=path]' or 'datacreator.js -O [path]'"
 }] ).run();
 
 var source = args.options.source;
 var output = args.options.output;
+var oldvalue = args.options.oldvalue;
+var title = args.options.title;
+var size = args.options.size;
+var year = args.options.year;
+var attr = args.options.attr;
+
 var tpsOpt = args.options.tps;
 var backward = args.options.backward;
 var basename = path.basename(source).split('.')[0].replace(/_points$/, '');
@@ -56,68 +91,80 @@ if (tpsOut) {
     tps = new ThinPlateSpline({});
 }
 
-var rs = fs.ReadStream(source);
-var rl = readline.createInterface({'input': rs, 'output': {}});
-var filebuf = null;
-var points = [];
-rl.on('line', function(line) {
-    if (!filebuf) {
-        if (line.indexOf('mapX,mapY,') == 0) {
-            if (bakOut) {
-                stop('Source file is already QGIS format.');
-            }
-            filebuf = 'qgis';
-        } else if (line.indexOf('{') == 0) {
-            bakIn = true;
-            if (!tpsOut && !bakOut) {
-                stop('Source file is already Maplat format.');
-            }
-            filebuf = line;
-        } else stop('Source file format is not match');
-    } else {
-        if (filebuf == 'qgis') {
-            var values = line.split(',').map(function(val){
-                return parseFloat(val);
-            });
-            var point = [[values[2], -1.0*values[3]], [values[0], values[1]]];
-            points.push(point);
-            if (tpsOut) tps.add_point(point[0], point[1]);
-        } else filebuf = filebuf + line;
+var content = fs.readFileSync(source);
+var sourcedata;
+if (content.indexOf('mapX,mapY,') == 0) {
+    if (bakOut) {
+        stop('Source file is already QGIS format.');
     }
-});
-rl.on('close', function(line) {
-    if (filebuf != 'qgis') {
-        var jval = JSON.parse(filebuf);
-        points = jval.gcps;
-        console.log(points);
-        if (tpsOut) tps.push_points(points);
+    sourcedata = parseCSV(content);
+} else if (content.indexOf('{') == 0) {
+    bakIn = true;
+    if (!tpsOut && !bakOut) {
+        stop('Source file is already Maplat format.');
     }
-    if (!bakOut) {
-        if (!bakIn) {
-            for (var i = 0; i < points.length; i++) {
-                if (i == 0) fs.writeFileSync(output, '[\n');
-                fs.appendFileSync(output, '    ' + JSON.stringify(points[i]));
-                if (i == points.length - 1) fs.appendFileSync(output, '\n]\n');
-                else fs.appendFileSync(output, ',\n');
-            }
+    sourcedata = parseJSON(content);
+} else stop('Source file format is not match');
+if (tpsOut) tps.push_points(sourcedata.gcps);
+
+if (oldvalue) {
+    var olddata = JSON.parse(fs.readFileSync(oldvalue));
+    olddata.gcps = sourcedata.gcps;
+    sourcedata = olddata;
+}
+if (title) sourcedata.title = title;
+if (size) {
+    console.log(size);
+    sourcedata.width = size[0];
+    sourcedata.height = size[1];
+}
+if (attr) sourcedata.attr = attr;
+if (year) sourcedata.year = year;
+
+if (!bakOut) {
+    if (!bakIn) {
+        var outdata = {};
+        for (var key of ['title', 'attr', 'width', 'height', 'year', 'gcps']) {
+            outdata[key] = sourcedata[key];
         }
-    } else {
-        for (var i=0; i<points.length; i++) {
-            if (i==0) fs.writeFileSync(bakOut, 'mapX,mapY,pixelX,pixelY,enable\n');
-            fs.appendFileSync(bakOut, points[i][1][0] + ',' + points[i][1][1] + ',' +
-                points[i][0][0] + ',' + (-1.0*points[i][0][1]) + ',1\n');
-        }
+        fs.writeFileSync(output, JSON.stringify(outdata, null, '  '));
     }
-    if (tpsOut) {
-        tps.solve();
-        fs.writeFile(tpsOut, new Buffer(tps.serialize()), function (err) {
-            if (err) stop(err);
-        });
+} else {
+    var points = sourcedata.gcps;
+    for (var i=0; i<points.length; i++) {
+        if (i==0) fs.writeFileSync(bakOut, 'mapX,mapY,pixelX,pixelY,enable\n');
+        fs.appendFileSync(bakOut, points[i][1][0] + ',' + points[i][1][1] + ',' +
+            points[i][0][0] + ',' + (-1.0*points[i][0][1]) + ',1\n');
     }
-});
-rl.resume();
+}
+if (tpsOut) {
+    tps.solve();
+    fs.writeFile(tpsOut, new Buffer(tps.serialize()), function(err) {
+        if (err) stop(err);
+    });
+}
 
 function stop(message) {
     console.log(message);
     process.exit(1);
+}
+
+function parseCSV(value) {
+    var res = csvSync(value, {
+        auto_parse: true, from: 2
+    });
+    var points = [];
+    for (var i = 0; i < res.length; i++) {
+        var values = res[i];
+        var point = [[values[2], -1.0*values[3]], [values[0], values[1]]];
+        points.push(point);
+    }
+    var ret = {
+        gcps: points
+    };
+    return ret;
+}
+
+function parseJSON(value) {
+    return JSON.parse(value);
 }
