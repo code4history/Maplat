726993012d1ae47a996ff33c551232ed50dc58e8
Kohei Otsuka <kohei.otsuka@here.com>
2017-06-27 13:19:12 +0900

    Testing imagecutter on Windows

--- editor/lib/imagecut.js
+++ editor/lib/imagecut.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var exec = require('child_process').exec;
+var path = require('path');
 
-var sourceFile = __dirname + '/../../original/morioka.jpg';
-var cutOutDirectory = __dirname + '/../out';
-var imageCutterTool = __dirname + '/../common/tools/imagecutter.js';
+var sourceFile = path.resolve(path.__dirname + '/../../original/morioka.jpg');
+var cutOutDirectory = path.resolve(__dirname + '/../out');
+var imageCutterTool = path.resolve(__dirname + '/../common/tools/imagecutter.js');
 
 var imagecut = {
     cutImageToTile: function(cb) {
-        exec(imageCutterTool + ' -s ' + sourceFile + ' -o ' + cutOutDirectory, function(err, stdout, stderr) {
+        console.log(imageCutterTool);
+        console.log(sourceFile);
+        exec('node ' + imageCutterTool + ' -s ' + sourceFile + ' -o ' + cutOutDirectory, function(err, stdout, stderr) {
             if (err) { console.log(err); }
             console.log(stdout);
         });
