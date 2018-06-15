a7bc0d2bb661be2c9fe52d39cf28ed2860934c36
OHTSUKA Ko-hei <kochizufan@gmail.com>
2018-06-15 23:42:49 +0900

    MaplatEditorの内部エラー回避用

--- js/histmap.js
+++ js/histmap.js
@@ -182,7 +182,11 @@ define(['ol-custom'], function(ol) {
             }, options))
                 .then(function(obj) {
                     return new Promise(function(resolve, reject) {
-                        if (!options.gcps || options.gcps.length < 3) resolve(obj);
+                        if (options.noload) {
+                            /* MaplatEditor用 なくさない */
+                            resolve(obj);
+                            return;
+                        }
                         obj.mapSize2MercSize(resolve);
                     });
                 }).catch(function(err) {
