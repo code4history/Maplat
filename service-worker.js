e15ddc5fddf962e5867f9ee69d625fb1a1de097a
OHTSUKA Ko-hei <kochizufan@gmail.com>
2018-08-28 02:12:54 +0900

    service-worker更新

--- service-worker.js
+++ service-worker.js
@@ -21,10 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "."
+    "revision": "Tue Aug 28 2018 01:59:53 GMT+0900 (JST)"
   },
   {
     "url": "dist/maplat.js",
-    "revision": "7ca514b650389a7fcee676a863906e96"
+    "revision": "f53bb52b59710df80bb7e989453c7a1f"
   },
   {
     "url": "dist/maplat.css",
