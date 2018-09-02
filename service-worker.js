454f9b4a53b109b1811a86a081bb75b4f6e293cb
OHTSUKA Ko-hei <kochizufan@gmail.com>
2018-09-02 15:28:59 +0900

    情報ウィンドウのタイトルがundefinedになるケースを修正 => build

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": ".",
-    "revision": "Sun Sep 02 2018 09:42:36 GMT+0900 (JST)"
+    "revision": "Sun Sep 02 2018 15:27:17 GMT+0900 (JST)"
   },
   {
     "url": "dist/maplat.js",
-    "revision": "76d047e3aa69f873918e36166600b2ad"
+    "revision": "76d8cb6503d0f9295cf9bf20b5142b42"
   },
   {
     "url": "dist/maplat.css",
