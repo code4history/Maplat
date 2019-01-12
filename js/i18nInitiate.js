d83a84ac44ae96f087d9dddbf8af65432dbbb3c5
OHTSUKA Ko-hei <kochizufan@gmail.com>
2019-01-12 18:14:26 +0900

    i18nクラス作成

---
+++ js/i18nInitiate.js
@@ -0,0 +1,14 @@
+define(['i18n', 'i18nxhr'],
+    function(i18n, i18nxhr) {
+    var i18Initiator = {};
+
+    i18Initiator.initWait = new Promise(function(resolve) {
+
+    });
+
+    i18Initiator.translate = function() {
+
+    };
+
+    return i18Initiator;
+});
