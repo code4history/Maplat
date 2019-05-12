7b41b7e7e66c91df7689a8eedf4b6439c7856952
Denis Zakharov <exxbrain@mail.ru>
2019-05-12 17:08:53 +0300

    Convert java lib-maplat to kotlin

--- mobile_android/lib-maplat/src/main/java/jp/tilemap/maplat/IMaplatStringCallbackHandler.java
+++
@@ -1,9 +0,0 @@
-package jp.tilemap.maplat;
-
-/**
- * Created by kokogiko on 2018/07/29.
- */
-
-public interface IMaplatStringCallbackHandler {
-    void callback(String value);
-}
---
+++ mobile_android/lib-maplat/src/main/java/jp/tilemap/maplat/IMaplatStringCallbackHandler.kt
@@ -0,0 +1,9 @@
+package jp.tilemap.maplat
+
+/**
+ * Created by kokogiko on 2018/07/29.
+ */
+
+interface IMaplatStringCallbackHandler {
+    fun callback(value: String)
+}
