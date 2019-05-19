93b585f846d7d6b859763901a197e3875d4ed229
OHTSUKA Ko-hei <kochizufan@gmail.com>
2019-05-20 01:44:23 +0900

    Multi Map最初の実装

--- js/histmap_tin.js
+++ js/histmap_tin.js
@@ -4,11 +4,13 @@ define(['histmap', 'tin'], function(ol, Tin) {
 
         ol.source.HistMap.call(this, options);
 
-        this.tin = new Tin({
+        this.tins = [new Tin({
             wh: [this.width, this.height],
             strictMode: options.strictMode,
-            vertexMode: options.vertexMode
-        });
+            vertexMode: options.vertexMode,
+            importance: 0,
+            priority: 0
+        })];
     };
     ol.inherits(ol.source.HistMap_tin, ol.source.HistMap);
 
@@ -22,27 +24,60 @@ define(['histmap', 'tin'], function(ol, Tin) {
             });
             ol.proj.addProjection(proj);
             ol.proj.addCoordinateTransforms(proj, 'EPSG:3857', function(xy) {
-                return obj.tin.transform(xy, false);
+                return obj.tins[0].transform(xy, false);
             }, function(merc) {
-                return obj.tin.transform(merc, true);
+                return obj.tins[0].transform(merc, true);
             });
             ol.proj.transformDirect('EPSG:4326', proj);
+            var prom;
             if (options.compiled) {
-                obj.tin.setCompiled(options.compiled);
-                resolve(obj);
+                obj.tins[0].setCompiled(options.compiled);
+                prom = Promise.resolve();
             } else {
-                obj.finalizeCreateAsync_(options.gcps, resolve);
+                obj.tins[0].setPoints(options.gcps);
+                prom = obj.tins[0].updateTinAsync();
             }
-        });
-    };
-
-    ol.source.HistMap_tin.prototype.finalizeCreateAsync_ = function(points, resolve) {
-        var self = this;
-        this.tin.setPoints(points);
-        this.tin.updateTinAsync()
-            .then(function() {
-                resolve(self);
+            prom.then(function() {
+                var proms;
+                if (options.sub_maps) {
+                    var promarray = options.sub_maps.map(function(sub_map, i) {
+                        var index = i + 1;
+                        var tin = obj.tins[index] = new Tin({
+                            bounds: sub_map.bounds,
+                            strictMode: options.strictMode,
+                            vertexMode: options.vertexMode,
+                            importance: sub_map.importance,
+                            priority: sub_map.priority
+                        });
+                        var proj = new ol.proj.Projection({
+                            code: 'Illst:' + obj.mapID + '#' + index,
+                            extent: [tin.xy[0], tin.xy[1], tin.wh[0], tin.wh[1]],
+                            units: 'm'
+                        });
+                        ol.proj.addProjection(proj);
+                        ol.proj.addCoordinateTransforms(proj, 'EPSG:3857', function(xy) {
+                            return obj.tins[index].transform(xy, false);
+                        }, function(merc) {
+                            return obj.tins[index].transform(merc, true);
+                        });
+                        ol.proj.transformDirect('EPSG:4326', proj);
+                        if (sub_map.compiled) {
+                            obj.tins[index].setCompiled(sub_map.compiled);
+                            return Promise.resolve();
+                        } else {
+                            obj.tins[index].setPoints(sub_map.gcps);
+                            return obj.tins[index].updateTinAsync();
+                        }
+                    });
+                    proms = Promise.all(promarray);
+                } else {
+                    proms = Promise.resolve();
+                }
+                proms.then(function() {
+                    resolve(obj);
+                });
             });
+        });
     };
 
     ol.source.HistMap_tin.prototype.xy2MercAsync_ = function(xy) {
