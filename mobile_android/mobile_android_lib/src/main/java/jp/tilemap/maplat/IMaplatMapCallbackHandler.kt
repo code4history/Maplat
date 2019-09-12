688f9604f79b164290cea7d3d306abd81ebb16a6
Kohei Otsuka <kochizufan@gmail.com>
2019-09-12 11:17:35 +0900

    Remove all mobile-related setting

--- mobile_android/mobile_android_lib/src/main/java/jp/tilemap/maplat/IMaplatMapCallbackHandler.kt
+++
@@ -1,9 +0,0 @@
-package jp.tilemap.maplat
-
-/**
- * Created by kokogiko on 2018/07/30.
- */
-
-interface IMaplatMapCallbackHandler {
-    fun callback(value: Map<String, Any>?)
-}
--- mobile_android/mobile_android_lib/src/main/java/jp/tilemap/maplat/IMaplatStringCallbackHandler.kt
+++
@@ -1,9 +0,0 @@
-package jp.tilemap.maplat
-
-/**
- * Created by kokogiko on 2018/07/29.
- */
-
-interface IMaplatStringCallbackHandler {
-    fun callback(value: String)
-}
