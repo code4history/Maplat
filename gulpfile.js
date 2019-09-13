934ab0369f0b0d114549f5cb79c4032a4097da27
Kohei Otsuka <kochizufan@gmail.com>
2019-09-13 11:50:54 +0900

    gulp/npmタスク整理中

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,68 @@
 var gulp = require('gulp'),
+    spawn = require('child_process').spawn,
     fs = require('fs-extra'),
     zip = require('gulp-zip');
 
+gulp.task('server', function() {
+    spawn('node', ['server.js'], {
+        stdio: 'ignore',
+        detached: true
+    }).unref();
+    return Promise.resolve();
+});
+
 gulp.task('zip', function() {
     try {
-        fs.removeSync('./web_set.zip');
+        fs.removeSync('./distribution.zip');
     } catch (e) {
     }
     try {
-        fs.removeSync('./web_set');
+        fs.removeSync('./distribution');
+    } catch (e) {
+    }
+    try {
+        fs.removeSync('./example.zip');
+    } catch (e) {
+    }
+    try {
+        fs.removeSync('./example');
     } catch (e) {
     }
 
-    fs.ensureDirSync('./web_set');
-    fs.copySync('./dist', './web_set/dist');
-    fs.copySync('./parts', './web_set/parts');
+    fs.ensureDirSync('./distribution');
+    fs.copySync('./dist', './distribution/dist');
+    fs.copySync('./parts', './distribution/parts');
+    fs.copySync('./fonts', './distribution/fonts');
+    fs.copySync('./locales', './distribution/locales');
+
+    return new Promise(function(resolve, reject) {
+        gulp.src(['./distribution/**/*'])
+            .pipe(zip('distribution.zip'))
+            .on('error', reject)
+            .pipe(gulp.dest('./'))
+            .on('end', resolve);
+    }).then(function() {
+        fs.moveSync('./distribution', './example');
+        fs.copySync('./index.html', './example/index.html');
+        fs.copySync('./apps', './example/apps');
+        fs.copySync('./maps', './example/maps');
+        fs.copySync('./pois', './example/pois');
+        fs.copySync('./tiles', './example/tiles');
+        fs.copySync('./img', './example/img');
+        fs.copySync('./pwa', './example/pwa');
+        fs.copySync('./tmbs', './example/tmbs');
+        fs.copySync('./service-worker.js', './example/service-worker.js');
+        return new Promise(function(resolve, reject) {
+            gulp.src(['./example/**/*'])
+                .pipe(zip('example.zip'))
+                .on('error', reject)
+                .pipe(gulp.dest('./'))
+                .on('end', resolve);
+        });
+    }).then(function() {
+        fs.removeSync('./example');
+    });
+
 
     return new Promise(function(resolve, reject) {
         gulp.src(['./web_set/*', './web_set/*/*'])
