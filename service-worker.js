e5d2fbca9cab1f18e367a77379da86480436c650
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-02-24 18:14:35 +0900

    All npm update

--- service-worker.js
+++ service-worker.js
@@ -25,15 +25,15 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [
   {
     "url": ".",
-    "revision": "4ac2b3124213fa47863c1915cc8ae9c532dd72a5"
+    "revision": "90d8f34fcf23696f88cce401d18f2243e65e989a"
   },
   {
     "url": "dist/maplat.js",
-    "revision": "58cc0331058db74a16ed4e7a9e3597b9"
+    "revision": "9dfcbd75177c17f0afc92e3b747fe7bc"
   },
   {
     "url": "dist/maplat.css",
-    "revision": "bf7df3bd0a117a9e94a75ac8de61ca85"
+    "revision": "2d67fbe4331b0d15df151abe8e1ea7b3"
   },
   {
     "url": "parts/all_right_reserved.png",
@@ -193,23 +193,23 @@ self.__precacheManifest = [
   },
   {
     "url": "locales/en/translation.json",
-    "revision": "0dd53e891cb14462d03632a211d7593b"
+    "revision": "51c737eb68e17da165fbf5da4cf05a8b"
   },
   {
     "url": "locales/ja/translation.json",
-    "revision": "4abfcf4d8d29b1ccc6a465f2e199fb09"
+    "revision": "00e22b59db6c62129ddca328320e7199"
   },
   {
     "url": "locales/ko/translation.json",
-    "revision": "ad0717663a4ff7bdb5e9788f459c06f6"
+    "revision": "5fafee636f68ae4e7322ebb9068de093"
   },
   {
     "url": "locales/zh-TW/translation.json",
-    "revision": "e1cd2a44f3b332ca442bc4ee7538bf67"
+    "revision": "e5b3837886875a774d74f91080d60ccb"
   },
   {
     "url": "locales/zh/translation.json",
-    "revision": "ba7302a1e53698a65ad4384c818445c8"
+    "revision": "3a1daeda323e89e1947dc7e071bf757e"
   },
   {
     "url": "fonts/clarenbd-webfont.woff",
