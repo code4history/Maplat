8cd3be8183aeac5b46f296528f9bea4a44f1b0a6
Kohei Otsuka <kochizufan@gmail.com>
2021-08-05 20:18:24 +0900

    0.10.1

--- gulpfile.js
+++ gulpfile.js
@@ -47,3 +47,14 @@ gulp.task("zip", async () => {
   });
   fs.removeSync("./example");
 });
+
+gulp.task("git_switch", async () => {
+  try {
+    fs.statSync('.git.open');
+    fs.moveSync('.git', '.git.keep');
+    fs.moveSync('.git.open', '.git');
+  } catch(e) {
+    fs.moveSync('.git', '.git.open');
+    fs.moveSync('.git.keep', '.git');
+  }
+});
