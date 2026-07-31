f5ce62df53d65b0921a8bcc9787184d9af94adc0
Kohei Otsuka <kochizufan@gmail.com>
2026-07-31 10:52:41 +0900

    m1-t4: サニタイズ層を含む dist の再生成

--- dist/maplat_ui.es.js
+++ dist/maplat_ui.es.js
@@ -1,4 +1,4 @@
-import { M as w } from "./index-Gb9lk1t-.js";
+import { M as w } from "./index-B4FUn5vW.js";
 typeof window < "u" && window.MaplatUi && window.MaplatUi.MaplatUi && (window.MaplatUi = window.MaplatUi.MaplatUi);
 export {
   w as MaplatUi
--- public/service-worker.js
+++ public/service-worker.js
@@ -3343,7 +3343,7 @@ This is generally NOT safe. Learn more at https://bit.ly/wb-precache`;
   registerRoute(
     /(?:maps\/.+\.json|pwa\/.+|pois\/.+\.json|apps\/.+\.json|tmbs\/.+\.jpg|images\/.+\.(?:png|jpg))$/,
     new StaleWhileRevalidate({
-      cacheName: `resourcesCache-${"2026-07-30-21-15"}`,
+      cacheName: `resourcesCache-${"2026-07-31-10-52"}`,
       plugins: [
         new ExpirationPlugin({
           maxAgeSeconds: 86400,
