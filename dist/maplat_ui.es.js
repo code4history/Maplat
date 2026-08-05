c8aea12d8d015c04ea611ff721de7cd8ded8c317
Kohei Otsuka <kochizufan@gmail.com>
2026-08-05 22:23:22 +0900

    m6-t5: merge 後 dist 再生成（t4 deriveGoogleTileOptions + t5 耐障害の双方を含む Core 正史で構築）

--- dist/maplat_ui.es.js
+++ dist/maplat_ui.es.js
@@ -1,4 +1,4 @@
-import { M as w } from "./index-Dgd3NaB6.js";
+import { M as w } from "./index-CBWdtMF8.js";
 typeof window < "u" && window.MaplatUi && window.MaplatUi.MaplatUi && (window.MaplatUi = window.MaplatUi.MaplatUi);
 export {
   w as MaplatUi
