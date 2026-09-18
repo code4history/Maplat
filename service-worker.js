8b8635dcd86165cac7584089b602e036d857419e
kochizufan <kochizufan@users.noreply.github.com>
2026-09-18 03:54:15 +0000

    deploy: 7a55cf970a48fe224bbfbdeae14246204edbb42b

--- service-worker.js
+++ service-worker.js
@@ -3343,7 +3343,7 @@ This is generally NOT safe. Learn more at https://bit.ly/wb-precache`;
   registerRoute(
     /(?:maps\/.+\.json|pwa\/.+|pois\/.+\.json|apps\/.+\.json|tmbs\/.+\.jpg|images\/.+\.(?:png|jpg))$/,
     new StaleWhileRevalidate({
-      cacheName: `resourcesCache-${"2026-09-18-02-30"}`,
+      cacheName: `resourcesCache-${"2026-09-18-03-54"}`,
       plugins: [
         new ExpirationPlugin({
           maxAgeSeconds: 86400,
